a7cc35ac21b2d91e219be4526e2f9c51b0ccc420
Atanas Kaynarov <akaynarov@gmail.com>
2025-09-06 17:47:11 +0300

    feat: add typeorm

--- bibwin-api/src/app.module.ts
+++ bibwin-api/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      // TODO: Use environment variables for these values
+      username: 'bibwin_user',
+      password: 'bibwin_pass',
+      database: 'bibwin_db',
+      autoLoadEntities: true,
+      synchronize: true, // Note: Set to false in production
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
