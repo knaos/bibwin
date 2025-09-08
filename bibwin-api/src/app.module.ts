37facd0b7decc5ab52ddc175736dae40a49f9a07
Atanas Kaynarov <akaynarov@gmail.com>
2025-09-08 09:56:41 +0300

    Revert "feat: add typeorm"
    This reverts commit a7cc35ac21b2d91e219be4526e2f9c51b0ccc420.

--- bibwin-api/src/app.module.ts
+++ bibwin-api/src/app.module.ts
@@ -1,22 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      // TODO: Use environment variables for these values
-      username: 'bibwin_user',
-      password: 'bibwin_pass',
-      database: 'bibwin_db',
-      autoLoadEntities: true,
-      synchronize: true, // Note: Set to false in production
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
